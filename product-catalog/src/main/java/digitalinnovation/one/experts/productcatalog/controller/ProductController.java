3f60a29d330921350fc17595fa31e7f566e0527e
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-07-26 11:17:44 -0300

    Refactoring controller methods

--- product-catalog/src/main/java/digitalinnovation/one/experts/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/digitalinnovation/one/experts/productcatalog/controller/ProductController.java
@@ -1,38 +1,37 @@
 package digitalinnovation.one.experts.productcatalog.controller;
 
 import digitalinnovation.one.experts.productcatalog.model.Product;
-import digitalinnovation.one.experts.productcatalog.repository.ProductRepository;
+import digitalinnovation.one.experts.productcatalog.services.ProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/product")
 public class ProductController {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductService productService;
 
     @RequestMapping(method = RequestMethod.POST)
     public Product create(@RequestBody Product product) {
-        return productRepository.save(product);
+        return this.productService.save(product);
     }
 
     @RequestMapping
     public Iterable<Product> list() {
-        return productRepository.findAll();
+        return this.productService.listAll();
     }
 
     @RequestMapping(value = "/{id}")
-    public Optional<Product> findById(@PathVariable("id") Integer id) {
-        return productRepository.findById(id);
+    public ResponseEntity<Product> findById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(this.productService.listById(id).orElseThrow());
     }
 
     @RequestMapping(value = "/name/{name}")
     public Iterable<Product> findByName(@PathVariable("name") String name) {
-        return productRepository.findByName(name);
+        return this.productService.listByName(name);
     }
 
 }
